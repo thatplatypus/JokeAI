a9e5f1eb29210a050833d80d1d10dd7ab27a63b3
Tom Brewer <tombrewer@Toms-MacBook-Pro.local>
2025-02-10 20:34:28 -0700

    Remove experimental config

--- joke-ai/next.config.ts
+++ joke-ai/next.config.ts
@@ -3,15 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   env: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-  },
-  experimental: {
-    fontLoaders: [
-      {
-        loader: '@next/font/google',
-        options: { subsets: ['latin'] }
-      },
-    ],
-  },
+  }
 };
 
 export default nextConfig;
